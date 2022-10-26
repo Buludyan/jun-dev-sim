0200e996673ae9cb71c566cfb78fdb81befd73ad
Arman Buludyan <buludian.arman@gmail.com>
2022-10-26 14:28:38 +0400

    some additional ui

--- src/front-end/components/header/Header.tsx
+++ src/front-end/components/header/Header.tsx
@@ -4,7 +4,17 @@ import './Header.scss';
 export const Header = () => {
   return (
     <div className="header">
-      <div className="header__inner"></div>
+      <div className="header__inner">
+        <div className="header__stats">
+          <div className="header__stat">
+            <div className="header__icon"></div>
+            <div className="header__value"></div>
+          </div>
+          <div className="header__stat"></div>
+          <div className="header__stat"></div>
+          <div className="header__stat"></div>
+        </div>
+      </div>
     </div>
   );
 };
